a174462fb3f5b4c7b2f114fedc4d2220d9981f3f
Oliviagiles09 <giles11@usf.edu>
2025-03-09 23:00:05 -0400

    Task 2 - Support Tickets Dynamic Addition

---
+++ cc_14.js
@@ -0,0 +1,32 @@
+// Task 2 
+function createSupportTicket(customerName, issueDescrption, priorityLevel) {
+  const ticket = document.createElement('div');
+  ticket.classList.add('ticket');
+  ticket.setAttribute('id', 'ticket-' + Date.now());
+
+cosnt ticketName = document.createElement('h2');
+  ticketName.textContent = customerName;
+
+const ticketDescription = document.createElement('p');
+  ticketDescription.textContent = issueDescription;
+
+const ticketPriority = document.createElement('p');
+  ticketPriority.textContent = 'Priority: ${prioritylevel}';
+  ticketpriority.classList.add(priorityLevel);
+
+const resolveButton = document.createElement('button');
+  resolveButton.textContent = 'Resolve';
+
+resolveButton.addEventListener('click'.() => {
+  ticket.remove();
+});
+
+ticket.appendChild(ticketName);
+    ticket.appendChild(ticketDescription);
+    ticket.appendChild(ticketPriority);
+    ticket.appendChild(resolveButton);
+
+    // Append ticket to the container
+    const ticketContainer = document.getElementById('ticketContainer');
+    ticketContainer.appendChild(ticket);
+}
